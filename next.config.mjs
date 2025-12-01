ee4e4f4d3f5a47054fb861e5bf8e46ad13345be3
ThryLox <83971708+ThryLox@users.noreply.github.com>
2025-12-01 13:18:23 -0500

    fix: upgrade three.js and config transpilePackages to fix build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /* config options here */
+    transpilePackages: ['react-force-graph-3d', '3d-force-graph', 'three-render-objects']
 };
 
 export default nextConfig;
